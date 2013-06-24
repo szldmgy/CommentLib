8f554232f40b0a66429c7c452b098d63cc3a95d9
Ju2ender <ju2ender@gmail.com>
2013-06-24 12:27:28 +0800

    修改 main 编码，分拆代码模板
    修改 main 编码为 UTF-8，使中文能正常显示。分拆原代码模板为 C# 和 jQuery 两部分。

---
+++ Snippets_CSharp.cs
@@ -0,0 +1,32 @@
+﻿//--------------------------------------------------------------
+// 
+// C#
+// 
+
+public void ProcessRequest(HttpContext ctx) {
+
+  string callback = string.Empty,
+         json = string.Empty,
+         item = string.Empty;
+
+  int pageIndex = 0;
+
+  List<OrderComment> orderList = null;
+
+  callback = ctx.Request.QueryString["callback"];
+
+  pageIndex = string.IsNullOrEmpty(ctx.Request.QueryString["pi"]) ? 1 : Convert.ToInt32(ctx.Request.QueryString["pi"]);
+
+  json = "{" +
+           "\"list\":[" + json + "]," +
+           "\"item\":\"" + item + "\"," +
+           "\"item\":\"" + item + "\"" +
+         "}";
+  
+  json = callback + "(" + json + ")";
+
+  ctx.Response.ContentType = "application/x-javascript";
+  ctx.Response.HeaderEncoding = Encoding.UTF8;
+  ctx.Response.ContentEncoding = Encoding.UTF8;
+  ctx.Response.Write(json);
+}
---
+++ Snippets_JavaScript.js
@@ -0,0 +1,26 @@
+﻿//--------------------------------------------------------------
+// 
+// jQuery
+// 
+
+$.ajax({
+  url: '',
+  type: 'GET',
+  async: false,
+  dataType: 'JSON',
+  success: function (data) {
+    
+  },
+  error: function (jqXHR, textStatus, error) {
+    alert(textStatus + ': ' + error);
+  }
+});
+
+$.getJSON('', {
+  data: ''
+}, function (data) { 
+    
+})
+.fail(function (jqXHR, textStatus, error) {
+  alert(textStatus + ': ' + error);
+});
