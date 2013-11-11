2d8fe4a32b4a755bcc84011e63ea1ae609562d79
Ju2ender <ju2ender@gmail.com>
2013-11-11 11:11:39 +0800

    提取修改、重构的公用署名。删除 C# 多余内容，保留文件后缀的不同。

--- comment/CSharp.cs
+++ comment/CSharp.cs
@@ -3,7 +3,6 @@
  * description
  * 
  * Created by Ju2ender on 2013-11-10 11:18:14.
- * Updated by Ju2ender on 2013-11-10 11:18:14.
  * Copyright (c) 2013 Ju2ender. All rights reserved.
  * Released under the MIT license.
  * 
@@ -31,27 +30,12 @@
 
 /*!
  * webService.asmx.cs
- * description
- * 
- * Created by Ju2ender on 2013-08-05 10:44:27.
- * Copyright (c) 2013 Ju2ender. All rights reserved.
- * Released under the MIT license.
  */
 
 /*!
  * userControl.ascx.cs
- * description
- * 
- * Created by Ju2ender on 2013-08-05 10:44:27.
- * Copyright (c) 2013 Ju2ender. All rights reserved.
- * Released under the MIT license.
  */
 
 /*!
  * httpHandler.ashx.cs
- * description
- * 
- * Created by Ju2ender on 2013-08-05 10:44:27.
- * Copyright (c) 2013 Ju2ender. All rights reserved.
- * Released under the MIT license.
  */
