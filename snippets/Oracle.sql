476b2135ecdec810c500dade3621af794bd67e7e
Ju2ender <ju2ender@gmail.com>
2014-04-18 22:39:02 +0800

    添加一些技巧。

--- snippets/Oracle.sql
+++ snippets/Oracle.sql
@@ -9,10 +9,8 @@ FROM (
            rownum rn
     FROM (
         SELECT * 
-        FROM ORDERS 
-        WHERE CustomerID LIKE 'A%'
-        ORDER BY OrderDate DESC, 
-                 ShippingDate DESC
+        FROM table_name
+        ORDER BY id DESC
     ) T
     WHERE rownum < ((pageNumber * pageSize) + 1)
 )
@@ -32,4 +30,50 @@ VALUES(TO_DATE('2014/03/30 21:18:30', 'yyyy/mm/dd hh24:mi:ss'));
 
 SELECT DISTINCT column_name
 from table_name
+
+----------------------------------------------------------------
+-- 进入表修改模式
+--
+
+SELECT *
+FROM table_name
+FOR UPDATE
+
+----------------------------------------------------------------
+-- 多字段排序
+--
+
+SELECT *
+FROM table_name
+ORDER BY columnA DESC, columnB ASC
+
+----------------------------------------------------------------
+-- 获得系统时间的前一天的年月日部分
+--
+
+select trunc(sysdate - 1)
+from dual
+
+----------------------------------------------------------------
+-- 外链子查询与 case 用法
+--
+
+SELECT DISTINCT s.SPECIFICATION_NAME, s.PKID, 
+case 
+    when sl.recordCount is null then 0
+    else sl.recordCount
+end as recordCount
+FROM TSPECIFICATION s
+LEFT OUTER JOIN TMATERIAL_TREE m 
+ON s.PKID = m.SPECIFICATION_ID 
+LEFT OUTER JOIN (
+    select DISTINCT SPECIFICATION_NAME, count(1) as recordCount
+    from TSEARCH_LOG
+    where trunc(added_time) = trunc(sysdate - 1)
+    group by SPECIFICATION_NAME
+    order by recordCount desc
+) sl
+ON TRIM(s.SPECIFICATION_NAME) = TRIM(sl.SPECIFICATION_NAME)
+WHERE s.VALID = 'T' 
+ORDER BY recordCount desc, SPECIFICATION_NAME asc
 
