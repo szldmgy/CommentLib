85ef1e85e788da9a726ca09e296fcb47894a4af6
Ju2ender <ju2ender@gmail.com>
2014-04-26 22:50:04 +0800

    Add SQL Server paging procedure.

--- snippets/SQL.sql
+++ snippets/SQL.sql
@@ -18,3 +18,41 @@ GO
 EXEC xp_cmdshell '"D:\\Program Files\\filename.exe"'
 GO
 
+----------------------------------------------------------------
+-- Paging
+-- 
+
+ALTER procedure [dbo].[sp_PagingSysUser]
+    @LoginName nvarchar(50) = null,
+    @Realname nvarchar(100) = null,
+    @IsAdmin bit = null,
+    @Status int = null,
+    @IsLock bit = null,
+    @pageNo int = 1,
+    @pageSize int = 20
+as
+begin
+    
+    SELECT *
+    FROM (
+        SELECT PKID,
+               LoginName,
+               Realname,
+             
+               IsAdmin,
+               [Status],
+               IsLock,
+               LastLoginTime,
+               ROW_NUMBER() OVER (ORDER BY PKID DESC) as rowNumber,
+               COUNT(1) OVER() as recordCount
+        FROM SysUser
+        WHERE LoginName like '%' + isnull(@LoginName, LoginName) + '%' -- 判断条件是否为空
+        and Realname like '%' + isnull(@Realname, Realname) + '%'
+        and isAdmin = isnull(@IsAdmin, isAdmin)
+        and [Status] = isnull(@Status, [Status])
+        and IsLock = isnull(@IsLock, IsLock)
+    ) T
+    WHERE rowNumber > (@pageNo - 1) * @pageSize
+    AND rowNumber <= @pageNo * @pageSize
+end
+
