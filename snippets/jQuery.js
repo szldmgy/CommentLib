3dee22c45985394001097b837f9e97fff24bd765
Ju2ender <ju2ender@gmail.com>
2014-06-09 10:47:31 +0800

    Add jQuery snippets.

---
+++ snippets/jQuery.js
@@ -0,0 +1,9 @@
+// When mouse leave an element, execute the function.
+$(selector).mouseleave(function() { });
+
+// Check an element has a CSS class.
+$(selector).hasClass("className");
+
+// Get an element's index of parent element.
+$(selector).index();
+
