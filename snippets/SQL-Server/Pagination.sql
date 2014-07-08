539eb581ac5ef79ccddfc6acdaa3eb9244a6ced8
Ju2ender <ju2ender@gmail.com>
2014-07-08 16:29:04 +0800

    Modifiy create date.

--- snippets/SQL-Server/Pagination.sql
+++ snippets/SQL-Server/Pagination.sql
@@ -3,7 +3,7 @@
  * v1.0.0
  * 通用分页存储过程。
  * 
- * Created by Ju2ender on 2013-08-05 10:44:27.
+ * Created by Ju2ender on 2014-07-08 16:28.
  * Copyright (c) 2013, 2014 Ju2ender. All rights reserved.
  * Released under the MIT license.
  * https://github.com/Ju2ender/CommentLib/blob/master/LICENSE
