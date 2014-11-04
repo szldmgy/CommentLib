812904aeece3870211e0536db06174bc85d6657b
Ju2ender <ju2ender@gmail.com>
2014-11-04 09:47:58 +0800

    Add some jQuery skill

--- snippets/jQuery.js
+++ snippets/jQuery.js
@@ -10,3 +10,11 @@ $(selector).index();
 // Name selector
 $("[name='elementName']")
 
+$(selector).addClass("className");
+$(selector).removeClass("className");
+
+// Modify or add an attribute to an element.
+$(selector).attr("attrName", "attrValue");
+
+// Remove an emelent's attribute.
+$(selector).removeAttr("attrName");
