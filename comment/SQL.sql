3e01caf57285aff7bae9536d3b78a9059bb94648
Ju2ender <ju2ender@gmail.com>
2015-06-16 15:18:29 +0800

    Modify comments, remove useless file.

--- comment/SQL.sql
+++ comment/SQL.sql
@@ -1,12 +1,3 @@
-/*!
- * filename.sql v1.0.0
- * description
- * 
- * Created by Ju2ender on Dec 3, 2014 at 12:00.
- * Copyright (c) 2013, 2014 Ju2ender. All rights reserved.
- * Released under the MIT license.
- * https://github.com/Ju2ender/CommentLib/blob/master/LICENSE
- */
 
 ----------------------------------------------------------------
 -- 
--- comment/common.c
+++ comment/common.c
@@ -1,3 +1,13 @@
+/*!
+ * filename 0.1.0
+ * description
+ * 
+ * Created by Ju2ender on Dec 3, 2014.
+ * Copyright (c) 2013, 2015 Ju2ender. All rights reserved.
+ * Released under the MIT license.
+ * https://website/LICENSE
+ */
+
 //--------------------------------------------------------------
 // 
 // codeBlock
